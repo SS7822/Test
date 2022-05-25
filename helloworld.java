a39a5d5fa43b10b24ad73017505853ac89e1ed53
SS7822 <104347131+SS7822@users.noreply.github.com>
2022-05-25 16:55:25 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -5,5 +5,6 @@ class HelloWorld
     public static void main(String args[])
     {
         System.out.println("welcome to jenkins");
+        System.out.println("welcome to git hub integration with jenkins");
     }
 }
