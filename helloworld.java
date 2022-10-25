45a0025f9eaa82344f2cc297f9b6443272067e02
SS7822 <srinivasachary.eidiko@gmail.com>
2022-10-25 10:26:57 +0530

    test

--- helloworld.java
+++ helloworld.java
@@ -4,13 +4,14 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-<<<<<<< HEAD
+// <<<<<<< HEAD
 	      System.out.println("Hello, World");
 
-=======
+// =======
         System.out.println("welcome to jenkins");
         System.out.println("welcome to git hub integration with jenkins");
         System.out.println("poll scm");
->>>>>>> 812e7831fe8fe7dfd2d5b886002388856013fdc4
+	System.out.println("oct 25");
+// >>>>>>> 812e7831fe8fe7dfd2d5b886002388856013fdc4
     }
 }
